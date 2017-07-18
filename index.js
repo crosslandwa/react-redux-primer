db5ba22c6df69337ea3c9b61344cf8e96dc87ff1
crosslandwa <crossland.w.a@gmail.com>
2017-07-18 18:46:52 +0100

    Refactor sayHallo function

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 setTimeout(
-  function() {
-    document.getElementById('app').innerHTML = 'Hallo Welt!'
+  function () {
+    document.getElementById('app').innerHTML = sayHallo()
   },
   2000
 )
+
+// What if this function was super complicated and we wanted to break it out to it's own file...
+function sayHallo () {
+  return 'Hallo Welt!'
+}
