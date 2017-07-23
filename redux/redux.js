0e01b9e2e67cb5dc55aafc2fd3e5bbd87c0a2beb
crosslandwa <crossland.w.a@gmail.com>
2017-07-23 13:29:32 +0100

    Comment code

--- redux/redux.js
+++ redux/redux.js
@@ -3,20 +3,28 @@ const ReactDOM = require('react-dom')
 
 const Redux = require('redux')
 const ReactReduxBindings = require('react-redux')
+const Provider = ReactReduxBindings.Provider
 
-function reducer (state = {holla: 'world'}, action) {
-  return state
-}
-
-const store = Redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
+// Our React app
 const App = () => (
   <div>Hello world Redux app</div>
 )
 
+// Redux store reducer logic
+function reducer (state = {holla: 'world'}, action) {
+  return state
+}
+
+// Redux store creation (with optional dev tools binding)
+const store = Redux.createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+// Start our React app, wrapped in a <Provider />
 ReactDOM.render(
-  <ReactReduxBindings.Provider store={store}>
+  <Provider store={store}>
     <App />
-  </ReactReduxBindings.Provider>,
+  </Provider>,
   document.getElementById('app')
 )
